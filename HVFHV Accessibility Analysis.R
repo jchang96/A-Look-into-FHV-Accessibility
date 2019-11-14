1461dc5f6aaafbabbc67e61c8359eea396b5bb5d
Jean Chang <25871837+jchang96@users.noreply.github.com>
2019-11-14 15:24:08 -0500

    Add files via upload

--- HVFHV Accessibility Analysis.R
+++ HVFHV Accessibility Analysis.R
@@ -2,7 +2,7 @@
 
 ################################################################
 # Author: Jean Chang
-# Date Created: 8/15/2019
+# Date created: 8/15/2019
 # Purpose: Create a report about FHV accessibility for Medium
 ################################################################
 
@@ -24,15 +24,15 @@ library(tidyverse)
 library(wesanderson)
 
 # set working directory
-loc = "H:/FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility"
+loc = "H:/[1] FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility"
 setwd(loc)
 
 ################################################################
 #             Read csv into R
 ################################################################
 
-df_june_2019 <- read.csv(file="H:/FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility/Data/2019-06_FHV_WAVs_on_the_road_2019-09-11_v01.csv", header=TRUE, sep=",")
-df_june_2018 <- read.csv(file="H:/FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility/Data/2018-06_FHV_WAVs_on_the_road_2019-09-11_v01.csv", header=TRUE, sep=",") 
+df_june_2019 <- read.csv(file="H:/[1] FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility/Data/2019-06_FHV_WAVs_on_the_road_2019-09-11_v01.csv", header=TRUE, sep=",")
+df_june_2018 <- read.csv(file="H:/[1] FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility/Data/2018-06_FHV_WAVs_on_the_road_2019-09-11_v01.csv", header=TRUE, sep=",") 
 
 ################################################################
 #             Data Cleaning
@@ -50,7 +50,7 @@ df_june_2018$Pickup_latitude <- as.numeric(as.character(df_june_2018$Pickup_lati
 ################################################################
 
 # Load borough boundaries data
-shape_files_2 <- "H:/FHV Accessibility/WAV Dispatch/Plots/Shape Files 2"
+shape_files_2 <- "H:/[1] FHV Accessibility/High-Volume/Plots/Shape Files 2"
 borough_boundaries <- readOGR(dsn=shape_files_2, layer="borough_boundaries")
 
 ################################################################
@@ -66,7 +66,7 @@ june_2019 <- ggmap(get_stamenmap(rbind(as.numeric(paste(geocode_OSM("New York Ci
         axis.text.x=element_blank(),
         axis.text.y=element_blank(),
         axis.ticks = element_blank(),
-        plot.title = element_text(size = 15, face = 'bold', hjust = 0.5)) +
+        plot.title = element_text(size = 30, face = 'bold', hjust = 0.5)) +
   geom_polygon(data=borough_boundaries, aes(x=long, y=lat, group=group, fill=NA), color = "black", fill=NA, size=0.5) +
   labs(x=NULL, y=NULL, 
        title='2019 Pickups',
@@ -75,7 +75,7 @@ june_2019 <- ggmap(get_stamenmap(rbind(as.numeric(paste(geocode_OSM("New York Ci
 
 # Save map as png
 ggsave(june_2019,
-       filename = ("H:/FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility/Plots/June_2019.png"),
+       filename = ("H:/[1] FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility/Plots/June_2019.png"),
        width = 15,
        height = 10,
        dpi = 300)
@@ -89,7 +89,7 @@ june_2018 <- ggmap(get_stamenmap(rbind(as.numeric(paste(geocode_OSM("New York Ci
         axis.text.x=element_blank(),
         axis.text.y=element_blank(),
         axis.ticks = element_blank(),
-        plot.title = element_text(size = 15, face = 'bold', hjust = 0.5)) +
+        plot.title = element_text(size = 30, face = 'bold', hjust = 0.5)) +
   geom_polygon(data=borough_boundaries, aes(x=long, y=lat, group=group, fill=NA), color = "black", fill=NA, size=0.5) +
   labs(x=NULL, y=NULL, 
        title='2018 Pickups',
@@ -98,7 +98,7 @@ june_2018 <- ggmap(get_stamenmap(rbind(as.numeric(paste(geocode_OSM("New York Ci
 
 # Save map as png
 ggsave(june_2018,
-       filename = ("H:/FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility/Plots/June_2018.png"),
+       filename = ("H:/[1] FHV Accessibility/Research Projects/A Look into For Hire Vehicles Accessibility/Plots/June_2018.png"),
        width = 15,
        height = 10,
        dpi = 300)
